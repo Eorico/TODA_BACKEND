dc546ab8d1563ad7b7828bbf013ffd12d0551fd5
N3ls0nBalye <jv232106@gmail.com>
2026-03-09 22:52:13 +0800

    nag lagay ng signup, user, Login.

---
+++ src/Services/auth_service.py
@@ -0,0 +1,44 @@
+from src.Models.user_model import User
+from src.Utils.password import Hash_password, verify_password
+from src.Utils.jwt_handler import create_token
+from fastapi import  HTTPException
+
+
+async def signup(data):
+    
+    existing_user =  await User.find_one(User.email == data.email)
+
+    if  existing_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    
+    user = User(
+        name=data.name,
+        email=data.email,
+        password=Hash_password(data.password),
+        role=data.role
+    )
+
+    await user.insert()
+
+    return {"messange": "User created successfully"}
+
+
+async def login(data):
+    user = await User.find_one(User.email == data.email)
+
+    if not user:
+        raise HTTPException(status_code=404, detail="User not Found")
+    
+    if not verify_password(data.password, user.password):
+        raise HTTPException(status_code=401, detail="Invalid Password")
+    
+    token = create_token({
+        "user_id": str(user.id),
+        "role": user.role
+
+    })
+
+    return {
+        "access_token": token,
+        "role": user.role
+    }
